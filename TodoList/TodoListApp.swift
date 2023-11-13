ada27b3350345befc5b6ead8e6c3085c282c5d68
Seobe <woodyms@nate.com>
2023-11-14 00:15:26 +0900

    [ADD] 프로젝트 추가

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by seobe22 on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
